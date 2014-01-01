1ad6929dcbb0d60addc962d133ad170b6a15eac4
Tatsuya <tatsuya.sh.tiny@gmail.com>
2014-01-02 02:04:22 +0900

    左右の時刻同期のずれを解消．epsファイルのpdf変換を自動化．

---
+++ log_process/eps2pdf/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Diagnostics;
+
+namespace eps2pdf
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Directory.SetCurrentDirectory(Environment.CurrentDirectory + "\\..\\..\\..\\gyro\\out");
+
+			List<string> filefullpaths = Directory.EnumerateFiles(Environment.CurrentDirectory, "*.eps", SearchOption.TopDirectoryOnly).ToList();
+
+			foreach (var item in filefullpaths)
+			{
+				Process.Start("epstopdf.pl", Path.GetFileName(@item));
+			}
+		}
+	}
+}
--- log_process/gyro/Program.cs
+++ log_process/gyro/Program.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using SharpPlot;
 using System.Diagnostics;
+using System.Threading;
 
 namespace gyro
 {
@@ -22,7 +23,7 @@ namespace gyro
 
 		private static List<SixAxisLog> logdata = new List<SixAxisLog>();
 
-		private static readonly double QUARRY_SPAN = 300;		// データ切り出し間隔[秒]
+		private static readonly double QUARRY_SPAN = 200;		// データ切り出し間隔[秒]
 
 		static void Main(string[] args)
 		{
@@ -43,19 +44,18 @@ namespace gyro
 						logdata[index].SetRecord(ParseDateTime(div_line[0]), Double.Parse(div_line[1]), Double.Parse(div_line[2]), Double.Parse(div_line[3]));
 					}
 				}
-				logdata[index].OffsetTime();
+				logdata[index].OffsetTime(ParseDateTime("2013-12-28 15:18:19.408600"));
 			}
-
-			foreach (var item in logdata)
-			{
-				WriteLog(item);
-			}
-
+			
 			Debug.Assert(logdata[0].Lable == "left", "データ名称が異なります");
 			Debug.Assert(logdata[1].Lable == "right", "データ名称が異なります");
 			List<string> left = WriteLog(logdata[0]);
 			List<string> right = WriteLog(logdata[1]);
-			PlotLog(left, right);
+			
+			DrawDivGraph(right,left);
+			DrawAllGraph(logdata[1].Lable + ".txt", logdata[0].Lable + ".txt");
+
+
 		}
 
 		static double ParseDateTime(string arg)
@@ -79,6 +79,16 @@ namespace gyro
 			if (!Directory.Exists(stOutputDir))
 			{
 				Directory.CreateDirectory(stOutputDir);
+			}
+
+			using (StreamWriter sw = new StreamWriter(Path.Combine(stOutputDir, log.Lable + ".txt")))
+			{
+				sw.WriteLine("#{0}\t{1}\t{2}\t{3}", "time[s]", "yaw[degree]", "pitch[degree]", "roll[degree]");
+				for (int i = 0; i < log.Count; i++)
+				{
+					var record = log.GetRecord(i);
+					sw.WriteLine("{0:f6}\t{1:e3}\t{2:e3}\t{3:e3}", record.Item1, record.Item2, record.Item3, record.Item4);
+				}
 			}
 
 			List<SixAxisLog> divlog = new List<SixAxisLog>();
@@ -107,53 +117,92 @@ namespace gyro
 			return filenames;
 		}
 
+		private static void DrawAllGraph(string data_filename_r, string data_filename_l)
+		{
+			string fig_filename = "6AxisLog.eps";
+			string data_file_path_r = Path.Combine(stOutputDir, data_filename_r);
+			string data_file_path_l = Path.Combine(stOutputDir, data_filename_l);
 
+			#region gnuplot
+			using (PlotStream gnuplot = new PlotStream())
+			{
+				gnuplot.Start();
+				gnuplot.ChangeDirectory(stOutputDir);
+				gnuplot.SetLineStyle(1, new LineStyle(1, 1, Color.Cyan,7,2));
+				gnuplot.SetLineStyle(2, new LineStyle(1, 1, Color.Green, 7, 2));
+				gnuplot.SetLineStyle(3, new LineStyle(1, 1, Color.Magenta, 7, 2));
+				gnuplot.SetLineStyle(4, new LineStyle(1, 1, Color.Orange, 7, 2));
 
-		private static void PlotLog(List<string> data_filenames_r, List<string> data_filenames_l)
+				gnuplot.SetLegendPosition(LegendPosition.left, LegendPosition.top);
+				gnuplot.SetLegendFont(24, "Helvetica");
+
+				gnuplot.SetXLabel("time [s]", 36, "Helvetica");
+				gnuplot.SetYLabel("angle[degrees]", 36, "Helvetica");
+
+				gnuplot.SetTicsFont(28, "Helvetica");
+				gnuplot.SetXRange(0, 3200);
+				gnuplot.SetXTics(0, 400, false);
+				gnuplot.SetYRange(-20, 20);
+				gnuplot.SetYTics(-20, 2);
+
+				gnuplot.SetMargin(7.5, 10.5, 1, 3.5);
+				gnuplot.SetGrid();
+
+				gnuplot.PlotFromFile(data_file_path_r, 1, 3, PlottingStyle.lines, 1, PlotAxis.x1y1, "right pitch");
+				gnuplot.ReplotFromFile(data_file_path_r, 1, 4, PlottingStyle.lines, 2, PlotAxis.x1y1, "right roll");
+				gnuplot.ReplotFromFile(data_file_path_l, 1, 3, PlottingStyle.lines, 3, PlotAxis.x1y1, "left pitch");
+				gnuplot.ReplotFromFile(data_file_path_l, 1, 4, PlottingStyle.lines, 4, PlotAxis.x1y1, "left roll");
+
+				gnuplot.SetTerminal(Terminal.postscript, 20, 14, 28, "Helvetica");
+				gnuplot.SetOutput(fig_filename);
+				gnuplot.Replot();
+				gnuplot.SetOutput();
+				gnuplot.Stream.WriteLine("unset style line");
+				gnuplot.Stream.WriteLine("set terminal wxt");
+			}
+			Thread.Sleep(1000);
+			#endregion
+		}
+
+		private static void DrawDivGraph(List<string> data_filenames_r, List<string> data_filenames_l)
 		{
 			int num = Math.Max(data_filenames_r.Count, data_filenames_l.Count);
-			
 
+			string fig_filename;
 			for (int i = 0; i < num; i++)
 			{
-				string fig_filename = "6AxisLog_" + i.ToString()+".eps";
 				string data_file_path_r = Path.Combine(stOutputDir, data_filenames_r[i]);
 				string data_file_path_l = Path.Combine(stOutputDir, data_filenames_l[i]);
 				double x_range_start = QUARRY_SPAN * i;
 				double x_range_end = QUARRY_SPAN * (i+1);
 
-				#region gnuplot
+				fig_filename = "6AxisLog_Pitch_" + i.ToString() + ".eps";
+				#region gnuplot_pitch
 				using (PlotStream gnuplot = new PlotStream())
 				{
 					gnuplot.Start();
 					gnuplot.ChangeDirectory(stOutputDir);
-					gnuplot.SetLineStyle(1, new LineStyle(1, 1, Color.Green));
-					gnuplot.SetLineStyle(2, new LineStyle(1, 1, Color.Blue));
-					gnuplot.SetLineStyle(3, new LineStyle(1, 1, Color.Red));
-					gnuplot.SetLineStyle(4, new LineStyle(1, 1, Color.Purple));
-					gnuplot.SetLineStyle(5, new LineStyle(1, 1, Color.Cyan));
-					gnuplot.SetLineStyle(6, new LineStyle(1, 1, Color.Black));
+					gnuplot.SetLineStyle(1, new LineStyle(1, 1, Color.Cyan, 6, 0.1));
+					gnuplot.SetLineStyle(2, new LineStyle(1, 1, Color.Magenta, 6, 0.1));
 
 					gnuplot.SetLegendPosition(LegendPosition.left, LegendPosition.top);
 					gnuplot.SetLegendFont(24, "Helvetica");
-					//gnuplot.SetLegendTitle("");
+					//gnuplot.SetLegendTitle("Pitch comparison");
 
 					gnuplot.SetXLabel("time [s]", 36, "Helvetica");
 					gnuplot.SetYLabel("angle[degrees]", 36, "Helvetica");
 
 					gnuplot.SetTicsFont(28, "Helvetica");
 					gnuplot.SetXRange(x_range_start, x_range_end);
-					gnuplot.SetXTics(x_range_start, QUARRY_SPAN/10, false);
-					gnuplot.SetYRange(-20, 20);
-					gnuplot.SetYTics(-20, 2);
+					gnuplot.SetXTics(x_range_start, QUARRY_SPAN / 10, false);
+					gnuplot.SetYRange(-18, 6);
+					gnuplot.SetYTics(-18, 2);
 
 					gnuplot.SetMargin(7.5, 10.5, 1, 3.5);
 					gnuplot.SetGrid();
 
 					gnuplot.PlotFromFile(data_file_path_r, 1, 3, PlottingStyle.lines, 1, PlotAxis.x1y1, "right pitch");
-					gnuplot.ReplotFromFile(data_file_path_r, 1, 4, PlottingStyle.lines, 2, PlotAxis.x1y1, "right roll");
-					gnuplot.ReplotFromFile(data_file_path_l, 1, 3, PlottingStyle.lines, 3, PlotAxis.x1y1, "left pitch");
-					gnuplot.ReplotFromFile(data_file_path_l, 1, 4, PlottingStyle.lines, 4, PlotAxis.x1y1, "left roll");
+					gnuplot.ReplotFromFile(data_file_path_l, 1, 3, PlottingStyle.lines, 2, PlotAxis.x1y1, "left pitch");
 
 					gnuplot.SetTerminal(Terminal.postscript, 20, 14, 28, "Helvetica");
 					gnuplot.SetOutput(fig_filename);
@@ -162,8 +211,47 @@ namespace gyro
 					gnuplot.Stream.WriteLine("unset style line");
 					gnuplot.Stream.WriteLine("set terminal wxt");
 				}
+				Thread.Sleep(1000);
 				#endregion
+				
 
+				fig_filename = "6AxisLog_Roll_" + i.ToString() + ".eps";
+				#region gnuplot_roll
+				using (PlotStream gnuplot = new PlotStream())
+				{
+					gnuplot.Start();
+					gnuplot.ChangeDirectory(stOutputDir);
+					gnuplot.SetLineStyle(1, new LineStyle(1, 1, Color.Green, 7, 2));
+					gnuplot.SetLineStyle(2, new LineStyle(1, 1, Color.Orange, 7, 2));
+
+					gnuplot.SetLegendPosition(LegendPosition.left, LegendPosition.top);
+					gnuplot.SetLegendFont(24, "Helvetica");
+					//gnuplot.SetLegendTitle("Roll comparison");
+
+					gnuplot.SetXLabel("time [s]", 36, "Helvetica");
+					gnuplot.SetYLabel("angle[degrees]", 36, "Helvetica");
+
+					gnuplot.SetTicsFont(28, "Helvetica");
+					gnuplot.SetXRange(x_range_start, x_range_end);
+					gnuplot.SetXTics(x_range_start, QUARRY_SPAN / 10, false);
+					gnuplot.SetYRange(-12, 12);
+					gnuplot.SetYTics(-12, 2);
+
+					gnuplot.SetMargin(7.5, 10.5, 1, 3.5);
+					gnuplot.SetGrid();
+
+					gnuplot.PlotFromFile(data_file_path_r, 1, 4, PlottingStyle.lines, 1, PlotAxis.x1y1, "right roll");
+					gnuplot.ReplotFromFile(data_file_path_l, 1, 4, PlottingStyle.lines, 2, PlotAxis.x1y1, "left roll");
+
+					gnuplot.SetTerminal(Terminal.postscript, 20, 14, 28, "Helvetica");
+					gnuplot.SetOutput(fig_filename);
+					gnuplot.Replot();
+					gnuplot.SetOutput();
+					gnuplot.Stream.WriteLine("unset style line");
+					gnuplot.Stream.WriteLine("set terminal wxt");
+				}
+				Thread.Sleep(2000);
+				#endregion
 			}
 
 
@@ -236,6 +324,19 @@ namespace gyro
 			for (int i = 0; i < time.Count; i++)
 			{
 				time[i] -= offset;
+			}
+		}
+
+		public void OffsetTime(double zero)
+		{
+			if (time.Count == 0)
+			{
+				throw new InvalidOperationException();
+			}
+
+			for (int i = 0; i < time.Count; i++)
+			{
+				time[i] -= zero;
 			}
 		}
 
